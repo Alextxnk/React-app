cbe8838aaaf9192e22bc129490716dea7295798f
Alextxnk <alexsolov766@gmail.com>
2022-09-28 08:34:19 +0300

    chore(pages)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,13 +3,15 @@ import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitia
 class MyDocument extends Document {
    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
       const initialProps = await Document.getInitialProps(ctx);
-      return { ...initialProps};
+      return { ...initialProps };
    }
 
    render(): JSX.Element {
       return (
          <Html lang="ru">
-            <Head />
+            <Head>
+               <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
+            </Head>
             <body>
                <Main />
                <NextScript />
