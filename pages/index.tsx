beb9ee1404b7961779b4bb8fbf2af5b6a9a621ca
Alextxnk <alexsolov766@gmail.com>
2022-09-28 14:20:56 +0300

    add Tag and P components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Htag } from '../components';
+import { Button, Htag, P, Tag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -7,6 +7,13 @@ export default function Home(): JSX.Element {
       <Htag tag='h1'>Text</Htag>
       <Button appearance='primary' arrow='right'>Button</Button>
       <Button appearance='ghost' arrow='right'>Button</Button>
+      <P size='l'>Большой</P>
+			<P>Средний</P>
+			<P size='s'>Маленький</P>
+			<Tag size='s'>Ghost</Tag>
+			<Tag size='m' color='red'>Red</Tag>
+			<Tag size='s' color='green'>Green</Tag>
+			<Tag color='primary'>Primary</Tag>
     </>
   );
 }
