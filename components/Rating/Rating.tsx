f722ef9c3d7c31517debc93ff440bb81b1fdd518
Alextxnk <alexsolov766@gmail.com>
2022-09-30 01:01:46 +0300

    add config

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,6 +1,7 @@
 import { RatingProps } from './Rating.props';
 import styles from './Rating.module.css';
 import cn from 'classnames';
+import StarIcon from './star.svg';
 import { useState } from 'react';
 
 export const Rating = ({ isEditable = false, rating, setRating, ...props }: RatingProps): JSX.Element => {
@@ -8,7 +9,11 @@ export const Rating = ({ isEditable = false, rating, setRating, ...props }: Rati
    const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>)); 
 
    const constructRating = (currentRating: number) => {
-
+      const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
+         return (
+            <StarIcon />
+         );
+      });
    };
 
    return (
